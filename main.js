b1d99aa242557396e6ba575dd2ee6fbee13bd645
Yariv Fruend <yarivf@wix.com>
2025-03-21 09:33:36 +0200

    added a log message

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Hello Gravity!");
+document.readyState === "loading"
+? document.addEventListener("DOMContentLoaded", doSomething)
+: init();
+
+function init() {
+    logLoadMessage();
+}
+
+function logLoadMessage() {
+    const appUrl = 'https://yarwix.github.io/gravity';
+    fetch(`${appUrl}/log-animation.svg`)
+    .then(response => response.text())
+    .then(svgXml => {
+        const svgDataUrl = `data:image/svg+xml;base64,${btoa(svgXml)}`;
+        console.log('%c ', `
+            background-image: url(${ svgDataUrl });
+            padding-top: 200px;
+            padding-left: 750px; 
+            background-size: contain;
+            background-position: center center;
+            background-repeat: no-repeat;
+        `);
+    });  
+}
