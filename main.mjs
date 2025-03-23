7c59d05a002c7734e202ec448d61273e8303a2dd
Yariv Fruend <yarivf@wix.com>
2025-03-23 20:38:34 +0200

    added app info module | Added local debug checking

--- main.mjs
+++ main.mjs
@@ -1,5 +1,7 @@
+import appInfo from './app-info.js';
+
 document.readyState === "loading"
-? document.addEventListener("DOMContentLoaded", doSomething)
+? document.addEventListener("DOMContentLoaded", init)
 : init();
 
 function init() {
@@ -7,18 +9,21 @@ function init() {
 }
 
 function logLoadMessage() {
-    const appUrl = 'https://yarwix.github.io/gravity';
+    const
+    isLocal = window.location.protocol === 'http' || 'https' ? true : false,
+    appUrl = isLocal ? 'http://127.0.0.1:8080' : 'https://yarwix.github.io/gravity';
     fetch(`${appUrl}/log-animation.svg`)
     .then(response => response.text())
     .then(svgXml => {
         const svgDataUrl = `data:image/svg+xml;base64,${btoa(svgXml)}`;
         console.log('%c ', `
             background-image: url(${ svgDataUrl });
-            padding-top: 200px;
-            padding-left: 750px; 
+            padding-top: 40px;
+            padding-left: 150px;
             background-size: contain;
             background-position: center center;
             background-repeat: no-repeat;
         `);
+        console.log(`V. ${appInfo.version}`);
     });  
 }
